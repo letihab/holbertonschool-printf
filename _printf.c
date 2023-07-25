4769f3e82f8321e6ffb05fdb20bf9f055e6c834b
letihab <letihabib1822@gmail.com>
2023-07-24 23:11:59 -0700

    our own printf function

--- _printf.c
+++ _printf.c
@@ -14,8 +14,8 @@ int _printf(const char *format, ...)
 		{'d', print_d},
 		{'i', print_d},
 		{'s', print_str},
-		{'R', print_rot13},
-		{'r', print_rev},
+		/*{'R', print_rot13},
+		  {'r', print_rev},*/
 	};
 
 	if (format == NULL)
@@ -30,29 +30,25 @@ int _printf(const char *format, ...)
 		find = 0;
 		if (format[i] == '%')
 		{
-			if (format[i + 1] == '\0')
-				format++;
 			for (j = 0; arguments[j].specifier; j++)
 			{
 				if (format[i + 1] == arguments[j].specifier)
 				{
 					count += arguments[j].print(list);
 					find = 1;
-					format++;
 					break;
 				}
 			}
-		}
 
-		if (find != 1)
-		{
-			if (*(format + i) == '%' )
+
+		if (!find && format[i + 1] != '\0')
 			{
-				count += write(1, "%", 1);
-				format++;
+				count += write(1, &format[i], 1);
 			}
-			else
-				count += write(1, (format + i), 1);
+		i++;
+		else
+		{
+			count += write(1, &format[i], 1);
 		}
 	}
 	va_end(list);
--- function_op.c
+++ function_op.c
@@ -9,7 +9,7 @@ int print_char(va_list list)
 {
 	char c;
 
-	c = (va_arg(list, int));
+	c = va_arg(list, int);
 	_putchar(c);
 
 	return (1);
@@ -22,123 +22,50 @@ int print_char(va_list list)
 */
 int print_str(va_list list)
 {
-	char *str;
-	int i;
+	const char *str;
+	int count = 0;
 
-	str = (va_arg(list, char *));
+	str = (va_arg(list, const char *));
 
 	if (str == NULL)
 		str = "(null)";
 
-	for (i = 0; str[i] != '\0'; i++)
+	while (*str)
 	{
-		_putchar(str[i]);
-	}
-
-	return (i);
-}
-
-/**
- * print_rot13 - a function that encodes a string using rot13 and
- * prrint to stdout
- *
- * @list: A list of argument of va_list type from stdarg.h
- * Return: void
- */
-int print_rot13(va_list list)
-{
-	char *s = va_arg(list, char*);
-	int i, j, count;
-	char *a = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char *b = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-
-	if (s == NULL)
-		return (0);
-
-	count = 0;
-	for (i = 0; *(s + i); i++)
-	{
-		for (j = 0; *(a + j); j++)
-		{
-			if (*(s + i) == *(a + j))
-			{
-				count += write(1, (b + j), 1);
-				break;
-			}
-		}
-	}
-		return (count);
-}
-/**
- * print_rev - prints string in reverse
- *
- * @list: A variable list of type va_list of stdarg.h
- * Return: Int - Number of characters printed
- */
-int print_rev(va_list list)
-{
-	int i, len, count;
-	char *s;
-
-	s = va_arg(list, char*);
-
-	if (*s == '\0')
-		return (0);
-
-	len = 0;
-
-	while (*(s + len))
-		len++;
-
-	count = 0;
-
-	for (i = len - 1; i >= 0; i--)
-	{
-		count += write(1, (s + i), 1);
+		_putchar(*str);
+		str++;
+			count++;
 	}
 
 	return (count);
+}
+
 /**
  * print_d - print a decimal number
  * @list: list of argument taken
  * Return: Number
 */
 int print_d(va_list list)
-{
-	unsigned int m;
-	int i, k, n = 0, count = 0;
+	int m, k, n, count = 0;
 
-	n = va_arg(list, int);
-	if (n <= INT_MAX && n >= INT_MIN)
-	{
-		if (n < 0)
+           n = va_arg(list, int);
+
+              if (n < 0)
 		{
-			n *= -1;
 			_putchar('-');
-			count += 1;
+			count ++ 1;
+			n = -n;
 		}
 		m = n;
-		for (k = 0; (m / 10) > 0; k++)
-			m = m / 10;
+		for (k = 1; m >= 10; k *= 10)
+			m /= 10;
 
-		m = n;
-		while (k != 0)
+		while (k > 0)
 		{
-			for (i = 0; i < k; i++)
-				m = m / 10;
-
-			m = m % 10;
-			_putchar(m + '0');
+			_putchar((n / k) + '0');
 			count++;
-			k--;
-			m = n;
+			n %= k;
+			k /= 10;
 		}
-		_putchar(m % 10 + '0');
-		count++;
-	}
-	else
-	{
-		return (-1);
-	}
 	return (count);
 }
