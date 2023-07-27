1d4160816de4b18847a63f69927979b1aff51e71
Kevin.P <kevin.pellissard@gmail.com>
2023-07-27 15:18:21 +0200

    fix from merge issue

--- main.h
+++ main.h
@@ -21,14 +21,11 @@ int print_str(va_list list);
 int print_char(va_list list);
 int print_d(va_list list);
 int _putchar(char c);
-<<<<<<< HEAD
-=======
 int convert_b(va_list list);
 unsigned int base_length(unsigned int num, int base);
 int convert_o(va_list list);
 int convert_hex(va_list list);
 int convert_HEX(va_list list);
 int hex_check(int num, char x);
->>>>>>> a4e171bcf7706d2f4fb84701256940c85b55634e
 
 #endif /* MAIN_H */
